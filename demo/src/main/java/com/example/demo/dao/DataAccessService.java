070de67ea42425315bb603c71beefb589cb07361
mau5073 <60010768+mau5073@users.noreply.github.com>
2021-04-14 17:26:41 -0400

    Add files via upload

--- demo/src/main/java/com/example/demo/dao/DataAccessService.java
+++ demo/src/main/java/com/example/demo/dao/DataAccessService.java
@@ -10,26 +10,44 @@ import java.util.HashMap;
 
 @Repository
 public class DataAccessService {
-    static HashMap<Integer, double[]> csvData = new HashMap<Integer, double[]>();
+    //Takes in a path to the csv file returns the size(number of rows that are in the file
+    static int  csvSize(String path) {
+        int size = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line= "";
+            while ((line = br.readLine()) != null) {
 
-    static public HashMap<Integer, double[]>  ReadData(String path){
+                size++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
+
+    static public double[][]  ReadData(String path){
+         int size = csvSize(path);
+         double[][] csvData= new double[size][];
         //x will be used to keep track of the row that is being read from the file that data needs to be taking in
         //starts at 1 so that consistency between csv file and data structure is made (1 in structure is 1 in csv)
-        int x = 1;
+        int x = 0;
         int rowLength = 0;
         String line= "";
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             while((line = br.readLine()) != null){
                 rowLength = (line.split(",")).length;
+                String [] rowData   = line.split(",");
                 // row will hold the data values in each row of the csv file
                 double[] row = new double[rowLength];
 
                 // loop through the row obtained by splitting line and enter it into the row array
                 for(int i = 0; i < rowLength; i++){
-                    row[i] = Double.parseDouble(line.split(",")[i]);
+                    row[i] = Double.parseDouble(rowData[i]);
                 }
                 //add the new row array to data entering the corresponding row number x that it came from in the file
-                csvData.put(x,row);
+                csvData[x]= row;
                 x++;
             }
         } catch(FileNotFoundException e){
